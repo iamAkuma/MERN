f92f3a1540fd2a4c4f775088ce5e1b9c49206091
Saral Bajracharya <rmanbajra@gmail.com>
2023-08-09 19:55:06 +0545

    Connected to MongoDB

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,8 +5,42 @@ import asyncHandler from "express-async-handler";
 // Route    POST /api/users/auth
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Auth User'})
+    res.status(200).json({ message: 'Auth User' })
+})
+
+// @desc    Registera new User
+// Route    POST /api/users/
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'Register User' })
 })
 
 
-export { authUser};
+// @desc   Logout User
+// Route    POST /api/users/logout
+// @access  Public
+const logoutUser = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'Logout User' })
+})
+
+
+// @desc    Get User profile
+// Route    GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'User Profile' })
+})
+
+// @desc    Update User Profile
+// Route    PUT /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'Update User Profile' })
+})
+export {
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile,
+};
