c5f832ece413f39fae351c0c3b30a6d5382a2693
Saral Bajracharya <rmanbajra@gmail.com>
2023-08-09 19:30:12 +0545

    Error Handler using middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,12 @@
+import asyncHandler from "express-async-handler";
+
+
 // @desc    Auth User/Set token
 // Route    POST /api/users/auth
 // @access  Public
-const authUser = (req, res) => {
+const authUser = asyncHandler(async (req, res) => {
     res.status(200).json({message: 'Auth User'})
-}
+})
 
 
 export { authUser};
