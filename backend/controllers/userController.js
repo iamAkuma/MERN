26be4aeadd4877a2199b58c71c8042b0794ba4f5
Saral Bajracharya <rmanbajra@gmail.com>
2023-08-10 18:58:44 +0545

    userModel added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,5 @@
 import asyncHandler from "express-async-handler";
-
+import User from "../models/userModel.js";
 
 // @desc    Auth User/Set token
 // Route    POST /api/users/auth
@@ -8,12 +8,33 @@ const authUser = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Auth User' })
 })
 
-// @desc    Registera new User
+// @desc    Register a new User
 // Route    POST /api/users/
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({email})
+
+    if(userExists){
+        res.status(400);
+        throw new Error('User already exists');
+    }
+
+    const user = await User.create({
+        name, email, password
+    })
+
+    if(user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        })
+    }
+
     res.status(200).json({ message: 'Register User' })
-})
+}) 
 
 
 // @desc   Logout User
