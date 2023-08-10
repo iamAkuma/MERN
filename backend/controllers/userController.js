ee168ee3492c61ca25ded47d402951aacdb2b5a9
Saral Bajracharya <rmanbajra@gmail.com>
2023-08-10 19:27:19 +0545

    Register amd Login routes added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,4 @@
+import generateToken from "../utils/generateToken.js";
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
 
@@ -5,7 +6,21 @@ import User from "../models/userModel.js";
 // Route    POST /api/users/auth
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Auth User' })
+ const {email, password} = req.body;
+
+ const user = await User.findOne({email});
+
+ if(user && (await user.matchPassword(password))) {
+    generateToken(res, user._id )
+    res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email
+    })
+} else{
+    res.status(401)
+    throw new Error('Invalid Email or Password!')
+}
 })
 
 // @desc    Register a new User
@@ -26,6 +41,7 @@ const registerUser = asyncHandler(async (req, res) => {
     })
 
     if(user) {
+        generateToken(res, user._id )
         res.status(201).json({
             _id: user._id,
             name: user.name,
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,7 +1,16 @@
-import { Jwt } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 const generateToken = (res, userId) => {
     const token = jwt.sign({ userId}, process.env.JWT_SECRET, {
         expiresIn: '30d'
     })
+
+    res.cookie('jwt', token, {
+        httpOnly : true,
+        secure   : process.env.NODE_ENV !== 'development', // set to https if production
+        sameSite: 'strict',
+        maxAge: 30 * 24 * 60 * 60 * 1000
+    })
 }
+
+export default generateToken;
