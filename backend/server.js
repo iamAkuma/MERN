38d0b284655e6362d5147fa3120169716bdfe502
Saral Bajracharya <rmanbajra@gmail.com>
2023-08-12 19:10:25 +0545

    Protected Routes using auth middleware

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ dotenv.config();
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import userRoutes from './routes/userRoutes.js'
 import connectDB from './config/db.js';
-
+import cookieParser from 'cookie-parser';
 connectDB();
 
 const port = process.env.PORT || 5000;
@@ -13,6 +13,8 @@ const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
+app.use(cookieParser())
 
 app.use('/api/users', userRoutes)
 
