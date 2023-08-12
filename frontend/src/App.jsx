f2cfd1d2b5d2e59d58ccc752ca0f1cf015d9f0e0
Saral Bajracharya <rmanbajra@gmail.com>
2023-08-12 20:08:58 +0545

    LinkContainer added for single page navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import Header from "./components/Header"
-import HomeScreen from "./screens/HomeScreen"
-
+import { Container } from "react-bootstrap"
+import { Outlet } from "react-router-dom"
 const App = () => {
   return (
     <>
       <Header />
-      <HomeScreen />
-     
-
+      <Container className='my-2'>
+      <Outlet />
+      </Container>
     </>
   )
 }
