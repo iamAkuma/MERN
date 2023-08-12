f1c361cffb46441262a9b84b058490305bd6e623
Saral Bajracharya <rmanbajra@gmail.com>
2023-08-12 19:50:50 +0545

    some boilerplate added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
-import Header
- from "./components/Header"
+import Header from "./components/Header"
+import HomeScreen from "./screens/HomeScreen"
+
 const App = () => {
   return (
     <>
-    <Header/>
-    <h1>MERN AUTH</h1>
-      
+      <Header />
+      <HomeScreen />
+     
+
     </>
   )
 }
