a929cdcd317887ac4469d573902c8c48f32922c0
Saral Bajracharya <rmanbajra@gmail.com>
2023-08-14 18:48:47 +0545

    Redux toolkit

--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -1,16 +1,22 @@
-import {createSlice} from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-    userInfo: localStorage.getItem('userinfo') ? JSON.parse(localStorage.getItem('userInfo'))   :  null
+    userInfo: localStorage.getItem('userinfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
 }
 
 const authSlice = createSlice({
     name: 'auth',
     initialState,
-    refucers: {
+    reducers: {
         setCredentials: (state, action) => {
-            state.userInfo=action.payload;
+            state.userInfo = action.payload;
             localStorage.setItem('userInfo', JSON.stringify(action.payload))
-        }
-    }
+        },
+        logout: (state, action) => {
+            state.userInfo = null;
+            localStorage.removeItem('userInfo')
+        },
+    },
 })
+export const {setCredentials, logout} = authSlice.actions;
+export default authSlice.reducer;
