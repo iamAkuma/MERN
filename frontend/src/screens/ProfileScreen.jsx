f8f06551842a6671a2e363eb2a17ead4c9fa73e7
Saral Bajracharya <rmanbajra@gmail.com>
2023-08-15 19:53:55 +0545

    MERN Auth App MADE

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -1,8 +1,7 @@
 import { useState, useEffect } from "react";
-import { Link, useNavigate} from 'react-router-dom'
 import { Form, Button } from 'react-bootstrap'
 import FormContainer from "../components/FormContainer";
-import {useDispatch, useSelector} from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { toast } from "react-toastify";
 import Loader from "../components/Loader";
 import { setCredentials } from "../slices/authSlice";
@@ -10,92 +9,92 @@ import { useUpdateUserMutation } from "../slices/usersApiSlice";
 
 
 const ProfileScreen = () => {
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setconfirmPassword] = useState('');
-
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-
-   
-    const {userInfo} = useSelector((state) => state.auth)
-
-    const [updateProfile, {isLoading}] = useUpdateUserMutation();
-
-    useEffect(() =>{
-     setName(userInfo.name);
-     setEmail(userInfo.email);
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setconfirmPassword] = useState('');
 
 
-    }, [userInfo.setName, userInfo.setEmail])
+  const dispatch = useDispatch();
 
-    const submitHandler = async (e) => {
-        e.preventDefault()
-        if(password !== confirmPassword) {
-            toast.error('Passwords donot match!');
-        } else {
-            try {
-             const res = await updateProfile({
-              _id: userInfo._id,
-              name,
-              email, 
-              password
-             }).unwrap();
-             dispatch(setCredentials({...res}));
-             toast.success('Profile updated')
-            } catch (error) {
-              toast.error(error?.data?.message || error.error)
-            }
-        }
+
+  const { userInfo } = useSelector((state) => state.auth)
+
+  const [updateProfile, { isLoading }] = useUpdateUserMutation();
+
+  useEffect(() => {
+    setName(userInfo.name);
+    setEmail(userInfo.email);
+
+
+  }, [userInfo.setName, userInfo.setEmail, userInfo.name, userInfo.email])
+
+  const submitHandler = async (e) => {
+    e.preventDefault()
+    if (password !== confirmPassword) {
+      toast.error('Passwords donot match!');
+    } else {
+      try {
+        const res = await updateProfile({
+          _id: userInfo._id,
+          name,
+          email,
+          password
+        }).unwrap();
+        dispatch(setCredentials({ ...res }));
+        toast.success('Profile updated')
+      } catch (error) {
+        toast.error(error?.data?.message || error.error)
+      }
     }
-    return (
-        <div>
-            <FormContainer>
-                <h1>Update Profile</h1>
-                <Form onSubmit={submitHandler}>
-                <Form.Group className="my-2" controlId='name'>
-                        <Form.Label>Enter Name</Form.Label>
-                        <Form.Control type='text'
-                            placeholder='Enter Name' value={name}
-                            onChange={(e) =>
-                                setName(e.target.value)}>
-                        </Form.Control>
-                    </Form.Group>
-                    <Form.Group className="my-2" controlId='email'>
-                        <Form.Label>Email Address</Form.Label>
-                        <Form.Control type='email'
-                            placeholder='Enter email' value={email}
-                            onChange={(e) =>
-                                setEmail(e.target.value)}>
-                        </Form.Control>
-                    </Form.Group>
+  }
+  return (
+    <div>
+      <FormContainer>
+        <h1>Update Profile</h1>
+        <Form onSubmit={submitHandler}>
+          <Form.Group className="my-2" controlId='name'>
+            <Form.Label>Enter Name</Form.Label>
+            <Form.Control type='text'
+              placeholder='Enter Name' value={name}
+              onChange={(e) =>
+                setName(e.target.value)}>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group className="my-2" controlId='email'>
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control type='email'
+              placeholder='Enter email' value={email}
+              onChange={(e) =>
+                setEmail(e.target.value)}>
+            </Form.Control>
+          </Form.Group>
 
-                    <Form.Group className="my-2" controlId='password'>
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type='password'
-                            placeholder='Enter Password' value={password}
-                            onChange={(e) =>
-                                setPassword(e.target.value)}>
-                        </Form.Control>
-                    </Form.Group>
+          <Form.Group className="my-2" controlId='password'>
+            <Form.Label>Password</Form.Label>
+            <Form.Control type='password'
+              placeholder='Enter Password' value={password}
+              onChange={(e) =>
+                setPassword(e.target.value)}>
+            </Form.Control>
+          </Form.Group>
 
-                    <Form.Group className="my-2" controlId='confirmpassword'>
-                        <Form.Label>Confirm Password</Form.Label>
-                        <Form.Control type='password'
-                            placeholder='Confirm Password' value={confirmPassword}
-                            onChange={(e) =>
-                                setconfirmPassword(e.target.value)}>
-                        </Form.Control>
-                    </Form.Group>
-                    {isLoading && <Loader/>}
-                    <Button type='submit' variant='primary' className="mt-3">
-                   Update
-                    </Button>                                     
-                </Form>
-            </FormContainer>
-        </div>
-    )
+          <Form.Group className="my-2" controlId='confirmpassword'>
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control type='password'
+              placeholder='Confirm Password' value={confirmPassword}
+              onChange={(e) =>
+                setconfirmPassword(e.target.value)}>
+            </Form.Control>
+          </Form.Group>
+          {isLoading && <Loader />}
+          <Button type='submit' variant='primary' className="mt-3">
+            Update
+          </Button>
+        </Form>
+      </FormContainer>
+    </div>
+  )
 }
 
 export default ProfileScreen
