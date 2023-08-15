547088963b84252fe8006775f9dccaad847fbf34
Saral Bajracharya <rmanbajra@gmail.com>
2023-08-15 19:15:17 +0545

    Update Profile UI added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,22 +9,27 @@ import './index.css'
 import HomeScreen from './screens/HomeScreen.jsx'
 import LoginScreen from './screens/LoginScreen.jsx'
 import RegisterScreen from './screens/RegisterScreen.jsx'
-
+import ProfileScreen from './screens/ProfileScreen.jsx'
+import PrivateRoute from './components/PrivateRoute.jsx'
 
 const router = createBrowserRouter(
-  createRoutesFromElements( 
-    <Route path= '/' element={<App/>}>
-    <Route index={true} path='/' element={<HomeScreen/>} />
-    <Route  path='/login' element={<LoginScreen/>} />
-    <Route  path='/register' element={<RegisterScreen/>} />
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/login' element={<LoginScreen />} />
+      <Route path='/register' element={<RegisterScreen />} />
+      {/*Private Routes */}
+      <Route path='' element={<PrivateRoute />}>
+        <Route path='/profile' element={<ProfileScreen />} />
+      </Route>
     </Route>
   )
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-  <React.StrictMode>
-   <RouterProvider router={router}/>
-  </React.StrictMode>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
   </Provider>
 )
